cc83d7a606d6624a27980812cfa4b47f73ae4c48
icedike <icedike@gmail.com>
2020-09-08 23:40:14 +0800

    feat: query data for home route

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const route = express.Router()
+const db = require('../../models')
+const Todo = db.Todo
+const User = db.User
 
-route.get('/', (req, res) => {
-  res.render('index')
+route.get('/', async (req, res) => {
+  try {
+    const todos = await Todo.findAll({
+      raw: true,
+      nest: true
+    })
+    res.render('index', { todos })
+  } catch (error) {
+    console.log(error)
+  }
 })
 
 module.exports = route
